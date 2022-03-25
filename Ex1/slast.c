62cfdad1c81f045bbe904828007e22a3e83100ed
Itay Sharabi <itay.nextweb@gmail.com>
2022-03-25 20:17:30 +0300

    Add files via upload

--- Ex1/slast.c
+++ Ex1/slast.c
@@ -8,64 +8,72 @@
 #include <time.h>
 
 
-void show_entry(struct utmp *entry_p) {
-
-    if (entry_p->ut_type == RUN_LVL) {
-        return; // Discard run_level system log in
-    }
-
-    printf("%-8.8s | \t", entry_p->ut_name);
-    printf("%d | \t", entry_p->ut_type);
-    printf("%d | \t", entry_p->ut_pid);
-
-    printf("%-8.8s | \t", entry_p->ut_id);
-
-    if (strcmp(entry_p->ut_line, "~") == 0) {
-        printf("system boot | \t");
-    }
-
-    else {
-        printf("%-8.8s | \t", entry_p->ut_line);
-    }
-
-    printf("%-8.16s | \t", entry_p->ut_host);
-    printf("%-8.20s | \t", ctime(&entry_p->ut_time));
-    printf("\n--------------------------------------------\n");
+void show_entry(struct utmp *utbuf_p) {
+	
+	printf("%-8.8s ", utbuf_p->ut_name); // Username
+	if (strcmp(utbuf_p->ut_line, "~") == 0) {
+		printf("system boot  ");
+		printf("%-8.16s ", utbuf_p->ut_host); // Host name
+	}
+	else {
+		printf("%-8.8s     ", utbuf_p->ut_line); // Command line name
+		printf("%-8.16s         ", utbuf_p->ut_host); // Host name
+	}
+	
+	long c = (utbuf_p->ut_time);
+	printf("%.16s \n", ctime(&c));
 }
 
 
 int main(int argc, char* argv[]) {
 
-
-    if (argc > 2 || argc == 1) {
-        printf("Usage: ./slast integer\n");
-        exit(-1); // Bad input error code
-    }
-
-    int X = atoi(argv[1]);
-
-    if (X <= 0) {
-        printf("Usage: ./slast integer\n");
-        exit(-1); // Bad input error code
-    }
-
-    int fd = open("/var/log/wtmp", O_RDONLY);
-
-    if (fd < 0) {
-        perror("File descriptor"); // Prints real error description using errno.h
-        exit(-2); // Bad file input
-    }
-
-    struct utmp current_record;
-
-    printf("Struct utmp size: %d\n", sizeof(current_record));
-
-    int size = sizeof(current_record);
-
-    while (read(fd, &current_record, size) == size) {
-        show_entry(&current_record);
-    }
-
-    printf("\n-----------------------\n Finished reading... \n----------------------------------\n");
-
+	if (argc > 2 || argc == 1) {
+	
+		printf("Usage: ./tool integer\n");
+		exit(-1); // Bad input error code
+	}
+	
+	int X = atoi(argv[1]);
+	if (X < 0) {
+		printf("Bad input!\nUsage: ./tool integer\nYou have entered X=%d\n", X);
+		exit(-1); // Bad input
+	}
+	
+	int fd = open("/var/log/wtmp", O_RDONLY);
+	
+	if (fd < 0) {
+		
+		perror("File descriptor");
+		exit(-2); // Bad file input
+	}
+	
+	struct utmp current_record;
+	
+	size_t size = sizeof(current_record);
+	off_t offset = lseek(fd, -size, SEEK_END);
+	
+	int finished_reading = 0;
+	
+	while (X > 0 && offset >= 0) {
+	
+		read(fd, &current_record, size);
+		if (current_record.ut_type != RUN_LVL) {
+			show_entry(&current_record);
+			X--;
+		}
+		offset = lseek(fd, -2*size, SEEK_CUR); // Move 2 time the size of the utmp struct
+		
+		if (offset <= size) { 
+			finished_reading = 1;
+		}
+	}
+        
+        if (!finished_reading) {
+        	lseek(fd, 0, SEEK_SET); // Go to beginning of the file.
+        	read(fd, &current_record, size);
+        }
+	time_t time_created = current_record.ut_time;
+	printf("\nwtmp begins ");
+	printf("%.24s\n", ctime(&time_created));
+	return 0;
 }
