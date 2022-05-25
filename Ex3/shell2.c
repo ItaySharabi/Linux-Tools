e5f2e738470fe434f326ce1a3dc9adede9b033c4
Itay Sharabi <itay.nextweb@gmail.com>
2022-05-26 00:31:08 +0300

    Makefile added. Question 1 implementation.

--- Ex3/shell2.c
+++ Ex3/shell2.c
@@ -11,20 +11,23 @@ int main() {
 char command[1024];
 char *token;
 char *outfile;
-int i, fd, amper, redirect, retid, status;
+int i, fd, amper, redirect_STD_OUT = 0, redirect_STD_ERR = 0, retid, status, size;
 char *argv[10];
 
 while (1)
 {
     printf("hello: ");
     fgets(command, 1024, stdin);
+    printf("Command: %s\n\n", command);
     command[strlen(command) - 1] = '\0';
+    size = strlen(command);
 
     /* parse command line */
     i = 0;
     token = strtok (command," ");
     while (token != NULL)
     {
+        // printf("Token: %s\n", token);
         argv[i] = token;
         token = strtok (NULL, " ");
         i++;
@@ -43,25 +46,42 @@ while (1)
     else 
         amper = 0; 
 
-    if (! strcmp(argv[i - 2], ">")) {
-        redirect = 1;
+    /* Redirections */
+    if (! strcmp(argv[i - 2], "2>")) {
+        redirect_STD_ERR = 1;
         argv[i - 2] = NULL;
         outfile = argv[i - 1];
-        }
-    else 
-        redirect = 0; 
+        // printf("Outfile: %s\n", outfile);
+    }
+
+    else if (! strcmp(argv[i - 2], ">")) {
+        redirect_STD_OUT = 1;
+        argv[i - 2] = NULL;
+        outfile = argv[i - 1];
+    }
 
     /* for commands not part of the shell command language */ 
 
     if (fork() == 0) { 
-        /* redirection of IO ? */
-        if (redirect) {
+        /* redirection of IO*/
+        if (redirect_STD_OUT) {
             fd = creat(outfile, 0660); 
             close (STDOUT_FILENO) ; 
             dup(fd); 
             close(fd); 
             /* stdout is now redirected */
-        } 
+        }
+        else if (redirect_STD_ERR) {
+            fd = creat(outfile, 0660); 
+            close (STDERR_FILENO) ; 
+            dup(fd); 
+            close(fd);
+            /* stderr is now redirected */
+        }
+        printf("command: %s\n", command);
+        for (int i = 0; i < strlen(command); ++i) {
+            printf("argv[%d] = %s\n", i, argv[i]);
+        }
         execvp(argv[0], argv);
     }
     /* parent continues here */
